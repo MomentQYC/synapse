c06475808816e8aedf71274fc477122f16b3dff5
MomentQYC <yateam@foxmail.com>
2023-09-24 22:40:39 +0800

    fix

--- synapse/version.py
+++ synapse/version.py
@@ -4,24 +4,20 @@ from matrix_common.versionstring import get_distribution_version_string
 
 from synapse.config.server import ServerConfig
 
-sv_config = ServerConfig()
-
 SYNAPSE_VERSION = get_distribution_version_string("matrix-synapse", __file__)
 
 
 class Python_Ver:
-    def __init__(self, sv_config) -> None:
-        self.config = sv_config
-
-    def getPythonVersion(self) -> str:
-        if not self.config.hide_python_version:
+    @classmethod
+    def getPythonVersion(cls) -> str:
+        if not ServerConfig.hide_python_version:
             return platform.python_version()
         else:
             return "UNKNOWN"
 
 
 def get_python_version() -> str:
-    return Python_Ver(sv_config).getPythonVersion()
+    return Python_Ver.getPythonVersion()
 
 
 PYTHON_VERSION = get_python_version()
