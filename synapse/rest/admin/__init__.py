b0075dad7207c9d71d1a81a1b7ea7c46afb865d5
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2023-09-25 00:59:27 +0800

    feat: Allow hiding the Python version

--- synapse/rest/admin/__init__.py
+++ synapse/rest/admin/__init__.py
@@ -107,9 +107,14 @@ class VersionServlet(RestServlet):
     PATTERNS = admin_patterns("/server_version$")
 
     def __init__(self, hs: "HomeServer"):
+        self.hs = hs
         self.res = {
             "server_version": SYNAPSE_VERSION,
-            "python_version": platform.python_version(),
+            "python_version": (
+                platform.python_version()
+                if not hs.config.server.hide_python_version
+                else "UNKNOWN"
+            ),
         }
 
     def on_GET(self, request: SynapseRequest) -> Tuple[int, JsonDict]:
